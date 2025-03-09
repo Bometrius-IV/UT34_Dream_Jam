104ab93c7a03bc69c3cdcabc9d63d67a598b9534
Bometrius-IV <Kenneth.shiller@gmail.com>
2025-03-09 17:48:16 -0400

    Spotlight function
    Added a spotlight and added logic when the player is in

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,24 @@ extends CharacterBody3D
 const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 const sens= 0.005
+@export var inLight= false;
 
 @onready var head = $head
 @onready var camera = $head/Camera3D
 
+func in_light(body: Node3D):
+	if body == self:
+		print_rich("in")
+		inLight=true
+
+func out_light(body: Node3D):
+	if body == self:
+		print_rich("out")
+		inLight=false
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	add_to_group("player")
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -21,20 +33,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		head.transform.basis
 		
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction : Vector3= (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if $trigger.stairs >0:
-			velocity.y=1
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
